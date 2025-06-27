b7aa683adc6b991acb2e4ac38f28e1a278d04f3a
gogogoghost <jaxyang39@gmail.com>
2025-06-27 18:32:41 +0800

    增加文档和注释

--- android.cpp
+++ android.cpp
@@ -2,16 +2,34 @@
 #include <jni.h>
 #include <map>
 
+// every instance map to a callback object
 std::map<jlong, jobject> callbackMap;
+
+void throwRuntimeException(JNIEnv *env, const char *message) {
+  jclass cls = env->FindClass("java/lang/RuntimeException");
+  env->ThrowNew(cls, message);
+}
+
 extern "C" {
-JNIEXPORT jlong JNICALL Java_com_gdszzy_blufi_Core_newCore(
-    JNIEnv *env, jobject obj, jint mtu, jobject onMessageCallback) {
+
+/**
+ * create new core
+ */
+JNIEXPORT jlong Java_com_gdszzy_blufi_Core_newCore(
+    JNIEnv *env, jobject obj,
+    // mtu
+    jint mtu,
+    // callback object has 'void onMessage(Byte,Byte,ByteBuffer)' method
+    jobject onMessageCallback) {
+  // convert to global
   jobject globalOnMessageCallback = env->NewGlobalRef(onMessageCallback);
   jclass callbackCls = env->GetObjectClass(globalOnMessageCallback);
   jmethodID onMessageMethod =
       env->GetMethodID(callbackCls, "onMessage", "(BBLjava/nio/ByteBuffer;)V");
+  // get vm
   JavaVM *vm;
   env->GetJavaVM(&vm);
+  // create instance
   blufi::Core *ptr = new blufi::Core(mtu, [=](uint8_t type, uint8_t subType,
                                               uint8_t *data, size_t size) {
     JNIEnv *subEnv;
@@ -26,12 +44,15 @@ JNIEXPORT jlong JNICALL Java_com_gdszzy_blufi_Core_newCore(
   return res;
 }
 
-JNIEXPORT void JNICALL Java_com_gdszzy_blufi_Core_freeCore(JNIEnv *env,
-                                                           jobject obj,
-                                                           jlong ptr) {
+/**
+ * free core
+ */
+JNIEXPORT void Java_com_gdszzy_blufi_Core_freeCore(JNIEnv *env, jobject obj,
+                                                   jlong ptr) {
   blufi::Core *core = (blufi::Core *)ptr;
+  // delete core
   delete(core);
-  // 删除全局引用callback
+  // delete callback
   auto it = callbackMap.find(ptr);
   if(it != callbackMap.end()) {
     jobject callback = it->second;
@@ -40,49 +61,78 @@ JNIEXPORT void JNICALL Java_com_gdszzy_blufi_Core_freeCore(JNIEnv *env,
   }
 }
 
-JNIEXPORT jbyte JNICALL Java_com_gdszzy_blufi_Core_onReceiveData(
-    JNIEnv *env, jobject obj, jlong ptr, jbyteArray bytes) {
+/**
+ * onReceiveData
+ */
+JNIEXPORT jbyte Java_com_gdszzy_blufi_Core_onReceiveData(JNIEnv *env,
+                                                         jobject obj, jlong ptr,
+                                                         jbyteArray bytes) {
   blufi::Core *core = (blufi::Core *)ptr;
   jsize len = env->GetArrayLength(bytes);
   jbyte *data = env->GetByteArrayElements(bytes, nullptr);
   return core->onReceiveData(std::span((uint8_t *)data, (size_t)len));
 }
 
-JNIEXPORT jobject JNICALL Java_com_gdszzy_blufi_Core_negotiateKey(JNIEnv *env,
-                                                                  jobject obj,
-                                                                  jlong ptr) {
+/**
+ * negotiateKey
+ */
+JNIEXPORT jobject Java_com_gdszzy_blufi_Core_negotiateKey(JNIEnv *env,
+                                                          jobject obj,
+                                                          jlong ptr) {
   blufi::Core *core = (blufi::Core *)ptr;
-  std::ignore = core->negotiateKey();
+  uint8_t ret = core->negotiateKey();
+  if(ret) {
+    throwRuntimeException(env, std::to_string(ret).c_str());
+  }
   auto buffer = core->getFlattenBuffer();
   return env->NewDirectByteBuffer(buffer.data(), buffer.size());
 }
-JNIEXPORT jobject JNICALL Java_com_gdszzy_blufi_Core_custom(JNIEnv *env,
-                                                            jobject obj,
-                                                            jlong ptr,
-                                                            jbyteArray bytes) {
+
+/**
+ * custom
+ */
+JNIEXPORT jobject Java_com_gdszzy_blufi_Core_custom(JNIEnv *env, jobject obj,
+                                                    jlong ptr,
+                                                    jbyteArray bytes) {
   blufi::Core *core = (blufi::Core *)ptr;
   jsize len = env->GetArrayLength(bytes);
   jbyte *data = env->GetByteArrayElements(bytes, nullptr);
-  std::ignore = core->custom(std::span((uint8_t *)data, len));
+  uint8_t ret = core->custom(std::span((uint8_t *)data, len));
+  if(ret) {
+    throwRuntimeException(env, std::to_string(ret).c_str());
+  }
   auto buffer = core->getFlattenBuffer();
   return env->NewDirectByteBuffer(buffer.data(), buffer.size());
 }
 
-JNIEXPORT jobject JNICALL Java_com_gdszzy_blufi_Core_scanWifi(JNIEnv *env,
-                                                              jobject obj,
-                                                              jlong ptr) {
+/**
+ * scanWifi
+ */
+JNIEXPORT jobject Java_com_gdszzy_blufi_Core_scanWifi(JNIEnv *env, jobject obj,
+                                                      jlong ptr) {
   blufi::Core *core = (blufi::Core *)ptr;
-  std::ignore = core->scanWifi();
+  uint8_t ret = core->scanWifi();
+  if(ret) {
+    throwRuntimeException(env, std::to_string(ret).c_str());
+  }
   auto buffer = core->getFlattenBuffer();
   return env->NewDirectByteBuffer(buffer.data(), buffer.size());
 }
 
-JNIEXPORT jobject JNICALL Java_com_gdszzy_blufi_Core_connectWifi(
-    JNIEnv *env, jobject obj, jlong ptr, jstring ssid, jstring pass) {
+/**
+ * connectWifi
+ */
+JNIEXPORT jobject Java_com_gdszzy_blufi_Core_connectWifi(JNIEnv *env,
+                                                         jobject obj, jlong ptr,
+                                                         jstring ssid,
+                                                         jstring pass) {
   blufi::Core *core = (blufi::Core *)ptr;
   const char *ssidLocal = env->GetStringUTFChars(ssid, nullptr);
   const char *passLocal = env->GetStringUTFChars(pass, nullptr);
-  std::ignore = core->connectWifi(ssidLocal, passLocal);
+  uint8_t ret = core->connectWifi(ssidLocal, passLocal);
+  if(ret) {
+    throwRuntimeException(env, std::to_string(ret).c_str());
+  }
   auto buffer = core->getFlattenBuffer();
   return env->NewDirectByteBuffer(buffer.data(), buffer.size());
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,60 @@
 ## blufi-core
 
-This library for development of js and miniprogram.
+This library is mainly to simplify the use of blufi.
+
+The core written by C++ and can be compiled to WebAssembly and JNI library.
+
+Not it support:
+
+- web (WebAssembly+JavaScript)
+- Android (JNI)
+
+### Usage (WebAssembly+JavaScript)
+
+```js
+import blufiLoader from './blufi.js'
+import blufiWasmUrl from './blufi.wasm?url'
+
+// Loading wasm module
+const blufiModule = (blufiModule = await blufiLoader({
+  // Using locateFile or any other way supported in Emscripten
+  locateFile: () => blufiWasmUrl
+}))
+
+// Scanning and connect to your BLE device
+connectBLEDevice()
+const blufiWriteChar = getBlufiWriteChar()
+const blufiNotifyChar = getBlufiReadChar()
+
+const core = new blufiModule.BlufiCore(
+  // mtu
+  128,
+  // onBlufiMessageCallback
+  (type, subType, buffer) => {
+    if (type == blufiModule.MsgType.VALUE) {
+      if (subType == blufiModule.MsgSubType.NEG) {
+        // Negotiate message
+        // You have to send the [buffer] to [blufiWriteChar]
+      }
+      // Other message process
+    }
+  }
+)
+
+// Enable notification
+blufiNotifyChar.oncharacteristicvaluechanged = (evt) => {
+  const buf = new Uint8Array(evt.currentTarget.value.buffer)
+  // Send all data received into core
+  // When a complete message received. onBlufiMessageCallback will be called
+  core.onReceiveData(buf)
+}
+blufiNotifyChar.startNotifications()
+
+// Initialization completed. Start to use core
+// All of these functions just help you to create the message you need send.
+// Then you have to sent them by yourself.
+const negotiateKeyMessage: Array<Uint8Array> = core.negotiateKey()
+core.scanWifi()
+core.connectWifi(ssid, password)
+core.custom(new Uint8Array([0xff, 0xaa]))
+```
